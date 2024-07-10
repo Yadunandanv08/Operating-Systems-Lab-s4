0479be0d9c0559fab71d4567f3d7fcb63bd7ab72
Yadunandan V <138135458+Yadunandanv08@users.noreply.github.com>
2024-07-10 10:42:06 +0530

    Create producerConsumer.c

---
+++ producerConsumer.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<semaphore.h>
+#include<pthread.h>
+#include<unistd.h>
+int buffer[10];
+sem_t mutex;
+sem_t full;
+sem_t empty;
+int in=0,out=0;
+void *producer(void *args){
+    for(int i=0;i<10;i++){
+        sleep(1);
+        int item;
+        sem_wait(&empty);
+        sem_wait(&mutex);
+        item=rand()%100;
+        buffer[in]=item;
+        in=(in+1)%10;
+        printf("produced %d\n",item);
+        sem_post(&mutex);
+        sem_post(&full);
+    }
+    
+}
+
+void *consumer(void *args){
+    for(int i=0;i<10;i++){
+        sleep(2);
+        int item;
+        sem_wait(&full);
+        sem_wait(&mutex);
+        item=rand()%100;
+        item=buffer[out];
+        in=(in+1)%10;
+        printf("consumed %d\n",item);
+        sem_post(&mutex);
+        sem_post(&empty);
+    }
+    
+}
+
+void main(){
+    sem_init(&mutex,0,1);
+    sem_init(&empty,0,10);
+    sem_init(&full,0,0);
+    
+    pthread_t p,c;
+    pthread_create(&p,NULL,producer,NULL);
+    pthread_create(&c,NULL,consumer,NULL);
+    
+    pthread_join(p,NULL);
+    pthread_join(c,NULL);
+    
+    
+}
